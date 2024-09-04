46840b668e5dc6235551beb3221b2ce0eac968df
Joshua Bauman <jbauman7@jh.edu>
2024-09-04 19:50:26 -0400

    added comments to todays afternoon exercises

--- day2_homework/gft2bed.py
+++ day2_homework/gft2bed.py
@@ -6,17 +6,17 @@ my_file = open(sys.argv[1])
 for my_line in my_file:
     if "##" in my_line: 
         continue
-    line_split = my_line.split("\t")
+    line_split = my_line.split("\t") #split the lines by tabs
     line8 = line_split[8]
-    line8_split = line8.split(";")
+    line8_split = line8.split(";") #split line 8 (gene names) by semicolon
     print(
-        line_split[0] + 
+        line_split[0] + #print the chromosome
         "\t" + 
-        line_split[3] + 
+        line_split[3] + #print the start position
         "\t" + 
-        line_split[4] +
+        line_split[4] + #print the stop position
         "\t" +
-        line8_split[2].rstrip("\"").lstrip("gene_name \"")
+        line8_split[2].rstrip("\"").lstrip("gene_name \"") #print the gene name (without "" or "gene_name")
         )        
 
 
--- day2_homework/tally-fixed.py
+++ day2_homework/tally-fixed.py
@@ -14,14 +14,14 @@ for my_line in my_file:
     if "#" in my_line:
         continue
     fields = my_line.split("\t")
-    if chr == "":
-        chr = fields[0]
+    if chr == "": 
+        chr = fields[0] #need to add this, as the original function was leaving line 1 of the data uncounted
     if fields[0] != chr:
         print( count, chr )
         chr = fields[0]
-        count = 1
+        count = 1 #Count now needs to be set to 1, so that it accounts for the count of line 1 that we added above
         continue
     count = count + 1
-print(count, chr)
+print(count, chr) #Prints the final entry ChrM, which was initially excluded by the if statement, because the loop only prints count,chr if it encounters a new chr value
 
 my_file.close()
