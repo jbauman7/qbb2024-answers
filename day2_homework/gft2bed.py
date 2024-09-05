40f9410461f26568d0cb53e2ca0f0708ffc63124
Joshua Bauman <jbauman7@jh.edu>
2024-09-05 09:42:01 -0400

    fixed gft2bed based on what we learned today

--- day2_homework/gft2bed.py
+++ day2_homework/gft2bed.py
@@ -10,14 +10,12 @@ for my_line in my_file:
     line8 = line_split[8]
     line8_split = line8.split(";") #split line 8 (gene names) by semicolon
     print(
-        line_split[0] + #print the chromosome
-        "\t" + 
-        line_split[3] + #print the start position
-        "\t" + 
-        line_split[4] + #print the stop position
-        "\t" +
-        line8_split[2].rstrip("\"").lstrip("gene_name \"") #print the gene name (without "" or "gene_name")
-        )        
+        [line_split[0]] + #print the chromosome
+        [line_split[3]] + #print the start position
+        [line_split[4]] + #print the stop position
+        [line8_split[2].rstrip("\"").lstrip("gene_name \"") #print the gene name (without "" or "gene_name")
+        ]  
+    )      
 
 
 my_file.close()
